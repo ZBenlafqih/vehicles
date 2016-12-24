1f64640b16686b1c656466519d6984c53066bb8e
ZBenlafqih <zakaria.benlafqih@gmail.com>
2016-12-24 00:36:02 +0000

    My proposition to solve sqli e-challenge
    /************
     The aim of the exercise is to pass all the test cases bellow.
     It's about vehicles and their consumption. Each vehicle has a different consumption according to its Fuel type :
      - Diesel: 5%
      - Gasoline: 6%
      - Hybrid: 3%
    
     There are three categories of vehicles :
      * CAR has 4 doors
      * TRUCK has 2 doors
      * MOTORCYCLE has no doors
    
     A vehicle with 4 doors is displayed as bellow. Please respect the door's numbers as in the schema :
             _
     door 1 | | door 2
     door 3 |_| door 4
    
    *************/

---
+++ src/main/java/com/sqli/vehicles/Vehicles.java
@@ -0,0 +1,43 @@
+package com.sqli.vehicles;
+
+/************
+ The aim of the exercise is to pass all the test cases bellow.
+ It's about vehicles and their consumption. Each vehicle has a different consumption according to its Fuel type :
+  - Diesel: 5%
+  - Gasoline: 6%
+  - Hybrid: 3%
+ 
+ There are three categories of vehicles : 
+  * CAR has 4 doors
+  * TRUCK has 2 doors
+  * MOTORCYCLE has no doors
+
+ A vehicle with 4 doors is displayed as bellow. Please respect the door's numbers as in the schema : 
+         _
+ door 1 | | door 2
+ door 3 |_| door 4
+
+*************/
+
+public class Vehicles {
+
+    public Vehicles(String vehicleDescriptions) {
+    	throw new UnsupportedOperationException("Still to be implemented");
+    }
+
+	public void move(String vehicleId, String distance) {
+    	throw new UnsupportedOperationException("Still to be implemented");
+    }
+
+	public String status(String vehicleId) {
+    	throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+	public String consumption(String vehicleId) {
+    	throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+    public void openDoor(String vehicleId, String doorNumber) {
+    	throw new UnsupportedOperationException("Still to be implemented");
+	}
+}
